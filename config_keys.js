230cd4116313c3047c4ad0ca7a629678236593e7
Furkan Yilmaz <mufuyil@gmail.com>
2016-05-17 12:41:06 -0700

    Proxy Server

---
+++ config_keys.js
@@ -0,0 +1,10 @@
+var config = {};
+
+var clientv4 = {};
+clientv4.apikey = "";
+clientv4.email = "";
+
+config.clientv4 = clientv4
+config.hostkey = "";
+
+module.exports = config;
---
+++ server.js
@@ -0,0 +1,92 @@
+var request = require('request')
+var querystring = require('querystring');
+var fs = require("fs");
+var express = require('express');
+var bodyParser = require('body-parser');
+var config = require('./config_keys');
+
+const PORT=8080; 
+
+var allowCrossDomain = function(req, res, next) {
+    if ('OPTIONS' == req.method) {
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+		res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+		res.sendStatus(200);
+    }
+    else {
+		res.writeHead(200, {
+			'Content-Type': 'text/plain',
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': '*'
+		});
+		next();
+    }
+};
+
+var app = express();
+app.use(bodyParser.json())
+app.use(allowCrossDomain);
+
+app.all('/proxy', function (req, res) {
+	console.log("*************************************************");	
+    console.log("START " + req.method + " " + req.url);
+    console.log("BODY" + JSON.stringify(req.body));
+    console.log("QUERY" + JSON.stringify(req.query));
+    console.log("PARAMS" + JSON.stringify(req.params));
+	
+	var url = req.url;
+	var proxyURL = req.query.proxyURL;
+	if (proxyURL === undefined) {
+		proxyURL = req.body.proxyURL;
+	}
+	console.log("URL: " + url + " proxyURL " + proxyURL);
+
+	if (req.method == "OPTIONS") {
+		res.send("myApi OPTIONS\n", {
+	        "Allow": "GET, OPTIONS",
+	        "Content-Type": "text/plain"
+	    }, 200);
+	}
+
+	if (proxyURL == "./config.js" || proxyURL == "./lang/en.js") {
+		// Handle static requests
+		var data = fs.readFileSync(proxyURL);
+		res.end(data.toString());
+	} else {
+		// Add host key to params
+		var params = JSON.parse(JSON.stringify(req.body));;
+		params.host_key = config.hostkey;
+		delete params.proxyURL;
+
+		console.log("Host " + JSON.stringify(params));
+
+		var options = {
+			method: req.method,
+			form: params,
+			json: true, 
+			url: proxyURL,
+			// headers: req.headers
+		}
+
+		request(options, function (err, newRes, body) {
+		  if (err) {
+		    console.log('Error :', err);
+		    res.end(body.toString());
+		    return
+		  }
+		  console.log('Success :', body);
+		  res.end(body.toString());
+		});		
+	}
+});
+
+app.post('/proxy', function (req, res) {
+	console.log("*************************************************");	
+    console.log("START " + req.method + " " + req.url);
+    // console.log("BODY" + JSON.stringify(req.body));
+});
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port ' + PORT);
+});
