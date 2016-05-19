f9d8b74266b44e6f7ded0c1c24c91c48871d045a
John Wineman <jwineman@cloudflare.com>
2016-05-19 13:29:45 -0700

    Adding abosluteUrlBase to config

--- src/js/actions/config.js
+++ src/js/actions/config.js
@@ -6,6 +6,8 @@ import { notificationAddError } from './notifications';
 import { isLoggedIn, getEmail } from '../utils/Auth/Auth';
 import { asyncUserLoginSuccess } from '../actions/user';
 
+const ABSOLUTE_URL_BASE = "absoluteUrlBase";
+
 export function configFetch() {
     return {
         type: ActionTypes.CONFIG_FETCH
@@ -33,6 +35,7 @@ export function asyncConfigFetch() {
         opts.headers = {'Accept': 'text/javascript'};
         http.get('./config.js', opts, function (response) {
                 let config = JSON.parse(response.text);
+                config = addAbsoluteUrlBase(config);
                 dispatch(configFetchSuccess(config));
                 dispatch(asyncIntlFetchTranslations(config.locale))
                 //log user in if their email is in local storage
@@ -46,4 +49,19 @@ export function asyncConfigFetch() {
                 dispatch(notificationAddError(error));
             });
     }
+}
+
+/*
+ * Some integrations don't work with relative paths because the URL doesn't match
+ * the actual file path, this function allows integrations to configure a base aboslute
+ * url path to be used in components/Image. absoluteBaseUrl should be defined globally
+ * on the page where the SPA is loaded.
+ */
+function addAbsoluteUrlBase(config) {
+    if(absoluteUrlBase) {
+        config[ABSOLUTE_URL_BASE] = absoluteUrlBase;
+    } else {
+        config[ABSOLUTE_URL_BASE] = "";
+    }
+    return config;
 }
