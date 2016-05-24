1da2927c6a93102eba071ca12a8746d7b341fe8b
John Wineman <jwineman@cloudflare.com>
2016-05-24 10:51:18 -0700

    Config reducer now handles CONFIG_UPDATE_BY_KEY

--- src/js/reducers/config.js
+++ src/js/reducers/config.js
@@ -20,6 +20,13 @@ export function configReducer(state = initialState, action) {
             return Object.assign({}, state, {
                 isFetching: false
             });
+        case ActionTypes.CONFIG_UPDATE_BY_KEY:
+            return Object.assign({}, state, {
+                    config: Object.assign({}, state.config, {
+                        [action.key]: action.value
+                    })
+                }
+            );
         default:
             return state;
     }
