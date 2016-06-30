31df5b70730fe0062720cc85e56d1df91a2e5989
Furkan Yilmaz <mufuyil@gmail.com>
2016-06-30 12:49:00 +0100

    added explanation for regex

--- src/js/actions/zonePurgeCache.js
+++ src/js/actions/zonePurgeCache.js
@@ -24,6 +24,8 @@ export function asyncZonePurgeCacheIndividualFiles(zoneId, files) {
     return dispatch => {
         dispatch(zonePurgeCache());
 
+        // Get an unstructured string like "     \nhttp://example.com\n\n  \n  http://example.com/hey \n "
+        // Return ["http://example.com", "http://example.com/hey"]
         var formatedFiles = files.replace(/^\s+|\s+$/g,'').split(/\s+/);
 
         v4ZonePurgeCache({zoneId: zoneId, files: formatedFiles}, function(response){
@@ -45,7 +47,7 @@ export function asyncZonePurgeCacheEverything(zoneId) {
         v4ZonePurgeCache({zoneId: zoneId, purge_everything: true}, function(response){
             if(v4ResponseOk(response)) {
                 dispatch(zonePurgeCacheSuccess());
-                dispatch(notificationAddSuccess("container.purgeCacheCard.success", true));
+                dispatch(notificationAddSuccess("container.purgeCacheByURLCard.success", true));
             } else {
                 dispatch(notificationAddClientAPIError(zonePurgeCacheError(), response));
             }
