ffc3c40af903f3ea4df5f5dafdd67cc9e7b1875c
Furkan Yilmaz <mufuyil@gmail.com>
2016-07-12 18:57:21 +0100

    added temporary home icon

--- src/js/containers/AppNavigation/AppNavigation.js
+++ src/js/containers/AppNavigation/AppNavigation.js
@@ -25,8 +25,8 @@ export default class AppNavigation extends Component {
                     <li className="icon-item">
                         <Link onClick={() => this.handleClick(UrlPaths.HOME_PAGE)}>
                             <span className="icon">
-                                <svg className="icon-svg" version="1.1" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" viewBox="0 0 40 40">
-                                    <path className="svg-main" d="M24,12h-8v-2h8V12z M30,12v18H10V12h4v2h12v-2H30z M16,23h-3v3h3V23z M16,18h-3v3h3V18z M27,23h-9v3h9V23z M27,18h-9v3h9V18z"></path>
+                                <svg className="icon icon-home" fill="#ffffff" version="1.1" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" viewBox="0 0 40 40">
+                                    <path className="svg-main" d="M32 18.451l-16-12.42-16 12.42v-5.064l16-12.42 16 12.42zM28 18v12h-8v-8h-8v8h-8v-12l12-9z"></path>
                                 </svg>
                             </span>
                             <span className="icon-title">
