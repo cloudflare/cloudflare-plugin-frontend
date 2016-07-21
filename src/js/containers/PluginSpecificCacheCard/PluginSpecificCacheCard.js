a6b78caf3dc3149293168b8d7a2a71de623e48cb
Furkan Yilmaz <mufuyil@gmail.com>
2016-07-21 14:51:18 +0100

    PI-647 changed PluginSpecificCacheCard toggle to a button, added modal

--- src/js/containers/PluginSpecificCacheCard/PluginSpecificCacheCard.js
+++ src/js/containers/PluginSpecificCacheCard/PluginSpecificCacheCard.js
@@ -4,13 +4,27 @@ import { FormattedMessage, injectIntl } from 'react-intl';
 import { asyncPluginUpdateSetting } from '../../actions/pluginSettings';
 import { getPluginSettingsValueForZoneId } from '../../selectors/pluginSettings';
 import { Card, CardSection, CardContent, CardControl, CardDrawers } from 'cf-component-card';
-import Toggle from 'cf-component-toggle';
+import { Modal, ModalHeader, ModalTitle, ModalClose, ModalBody, ModalFooter, ModalActions } from 'cf-component-modal';
+import { Button } from 'cf-component-button';
 
 const SETTING_NAME = "plugin_specific_cache";
 
 class PluginSpecificCacheCard extends Component {
+    state = {
+        isModalOpen: false,
+    };
+
+    handleModalOpen() {
+        this.setState({ isModalOpen: true });
+    }
+
+    handleModalClose() {
+        this.setState({ isModalOpen: false });
+    }
 
     handleChange(value) {
+        this.setState({ isModalOpen: false });
+
         let { activeZoneId, dispatch } = this.props;
         dispatch(asyncPluginUpdateSetting(SETTING_NAME, activeZoneId, value));
     }
@@ -25,10 +39,38 @@ class PluginSpecificCacheCard extends Component {
                             <p><FormattedMessage id="container.pluginSpecificCacheCard.description" /></p>
                         </CardContent>
                         <CardControl>
-                            <Toggle
-                                label=""
-                                value={(this.props.cacheCardValue === true)}
-                                onChange={this.handleChange.bind(this)}/>
+
+                            { (this.props.cacheCardValue === false) ? (
+                                <Button type="primary" onClick={this.handleModalOpen.bind(this)}>
+                                    <FormattedMessage id="container.pluginSpecificCacheCard.button.enable" />
+                                </Button> 
+                                ) :
+                                <Button onClick={this.handleChange.bind(this, false)}>
+                                    <FormattedMessage id="container.pluginSpecificCacheCard.button.disable" />
+                                </Button> 
+                            }
+
+                            <Modal
+                                isOpen={this.state.isModalOpen}
+                                onRequestClose={this.handleModalClose.bind(this)}>
+                                <ModalHeader>
+                                    <ModalTitle><FormattedMessage id="container.pluginSpecificCacheCard.modal.title"/></ModalTitle>
+                                    <ModalClose onClick={this.handleModalClose.bind(this)}/>
+                                </ModalHeader>
+                                <ModalBody>
+                                        <p><FormattedMessage id="container.pluginSpecificCacheCard.modal.description"/></p>
+                                </ModalBody>
+                                <ModalFooter>
+                                    <ModalActions>
+                                        <Button type="primary" onClick={ this.handleChange.bind(this, true) }>
+                                            <FormattedMessage id="container.pluginSpecificCacheCard.modal.button"/>
+                                        </Button>
+                                        <Button onClick={this.handleModalClose.bind(this)}>
+                                            <FormattedMessage id="container.pluginSpecificCacheCard.modal.buttonCancel"/>
+                                        </Button>
+                                    </ModalActions>
+                                </ModalFooter>
+                            </Modal>
                         </CardControl>
                     </CardSection>
                 </Card>
