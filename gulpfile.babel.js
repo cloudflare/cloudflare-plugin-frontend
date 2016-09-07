f29ad3981c52ff86c0c2ab7c8d9e17ed6832a443
Furkan Yilmaz <mufuyil@gmail.com>
2016-09-07 18:24:47 +0100

    PI-776 gulp calls eslint

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,6 +8,8 @@ import gutil from 'gulp-util';
 import watchify from 'watchify';
 import assign from 'lodash.assign';
 import wrap from 'gulp-wrap';
+import eslint from 'gulp-eslint';
+import plumber from'gulp-plumber';
 
 //custom browserify options
 var customOpts = {
@@ -23,6 +25,7 @@ var b = watchify(browserify(opts));
 //transformations
 b.transform(babelify);
 
+gulp.task('lint', lint); // so you can run `gulp lint` to lint the file
 gulp.task('js', bundle); // so you can run `gulp js` to build the file
 b.on('update', bundle); // on any dep update, runs the bundler
 b.on('log', gutil.log); // output build logs to terminal
@@ -37,8 +40,25 @@ function bundle() {
         // optional, remove if you don't need to buffer file contents
         .pipe(buffer())
         // optional, remove if you dont want sourcemaps
-        .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+        .pipe(sourcemaps.init({ loadMaps: true })) // loads map from browserify file
         // Add transformation tasks to the pipeline here.
         .pipe(sourcemaps.write('./')) // writes .map file
         .pipe(gulp.dest('./'));
+}
+
+function fixErrorHandling() {
+    return plumber({
+        errorHandler(err) {
+          gutil.log(err.stack);
+        }
+    });
+}
+
+function lint() {
+    return gulp.src('./src/**/*.js')
+        .pipe(fixErrorHandling())
+        .pipe(eslint())
+        .pipe(eslint.format())
+        // for some reason eslint doesn't work without this
+        .on('data', file => {});
 }
