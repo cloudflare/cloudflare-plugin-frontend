de89fdddae001c0bac98261a157032ce2c5824a4
Furkan Yilmaz <mufuyil@gmail.com>
2016-09-14 16:22:27 +0100

    PI-798 fixed www counting as subdomain

--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -14,10 +14,15 @@ function extractDomain(url) {
     return domain;
 }
 
+function beginsWith(needle, haystack){
+    return (haystack.substr(0, needle.length) == needle);
+}
+
 export function isSubdomain(selectedZoneName) {
     var currentDomainName = extractDomain(document.URL);
 
     if (currentDomainName.endsWith(selectedZoneName) &&
+        !beginsWith("www.", currentDomainName) &&
         selectedZoneName !== currentDomainName &&
         currentDomainName && selectedZoneName) {
         return true;
