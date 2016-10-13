5a4bf1152043b9b407796afcbbb5c4e4c8c1abb4
Furkan Yilmaz <mufuyil@gmail.com>
2016-10-13 12:58:00 +0100

    PI-866 fixed a bug where analytic link was using "?" instead of "&"

--- src/js/selectors/generateUTMLink.js
+++ src/js/selectors/generateUTMLink.js
@@ -1,7 +1,7 @@
 export function generateUTMLink(pageURL, utmSource, utmCampaign, utmContent) {
 	var utmLink = pageURL + '?utm_source=pi-' + utmSource + '&utm_medium=plugin&utm_campaign=' + utmCampaign;
 	if (utmContent !== undefined) {
-		utmLink += '?utm_content=' + utmContent;
+		utmLink += '&utm_content=' + utmContent;
 	}
 	
 	return utmLink;
