5815b9b34dd247a4b7bc0121c83800ad50a36110
Furkan Yilmaz <mufuyil@gmail.com>
2016-11-24 12:14:34 +0000

    PI-863 Removed unnecessary html element

--- src/js/containers/Header/Header.js
+++ src/js/containers/Header/Header.js
@@ -5,7 +5,6 @@ import { injectIntl } from 'react-intl';
 import ActiveZoneSelector from '../../containers/ActiveZoneSelector/ActiveZoneSelector';
 import { isLoggedIn } from '../../utils/Auth/Auth';
 import { getAbsoluteUrl } from '../../selectors/config';
-import { LayoutRow, LayoutColumn } from 'cf-component-layout';
 import UnderAttackButton from '../../containers/UnderAttackButton/UnderAttackButton';
 
 class Header extends Component {
@@ -17,19 +16,15 @@ class Header extends Component {
             <header id="header" className="header app-header">
                 <div className="gradient-bar-header"></div>
                 <div id="header-global" className="header-main">
-                    <div className="header-global-wrapper">
-                        <LayoutRow>
-                            <LayoutColumn width={1/8}>
-                                <img style={{ margin: "15px 0", width: "170px", height: "30px" }} src={ getAbsoluteUrl(config, 'assets/logo.svg') } />
-                            </LayoutColumn>
-                            <LayoutColumn width={1/8}>
-                                <ActiveZoneSelector/>
-                            </LayoutColumn>
-                            <LayoutColumn width={6/8}>
-                                { (isLoggedIn() && zoneSettings.entities[activeZone.id]) ? <UnderAttackButton/> : null }
-                            </LayoutColumn>
-                        </LayoutRow>
-                    </div>
+                    <LayoutColumn width={1/8}>
+                        <img style={{ margin: "15px 0", width: "170px", height: "30px" }} src={ getAbsoluteUrl(config, 'assets/logo.svg') } />
+                    </LayoutColumn>
+                    <LayoutColumn width={1/8}>
+                        <ActiveZoneSelector/>
+                    </LayoutColumn>
+                    <LayoutColumn width={6/8}>
+                        { (isLoggedIn() && zoneSettings.entities[activeZone.id]) ? <UnderAttackButton/> : null }
+                    </LayoutColumn>
                 </div>
             </header>
         );
