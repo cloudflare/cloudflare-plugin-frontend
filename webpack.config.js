6a80391e01cccd299fca2e8858f25ea809506310
John Wineman <john.wineman@gmail.com>
2017-02-06 07:48:27 -0800

    Migrate from Browserify to Webpack2 (#87)
    * Added webpack.
    
    * Added cheap-module-source-map source maps
    
    * Added eslint-loader
    
    * Added UglifyJS for production builds.
    
    * Added npm run build, lint, format, test.
    
    * Removed gulp, browserify.
    
    * Removed babel presets in webpack.config.js, moved webpack dependency local so we can version it.
    
    * Updated README to reflect new webpack tasks.

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack');
+
+var isDev = process.env.NODE_ENV !== 'production';
+var plugins = [];
+
+if(!isDev) {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+      compress: isDev,
+      mangle: false
+    })
+  );
+}
+
+module.exports = {
+  entry: "./src/js/index.js",
+  devtool: isDev ? 'cheap-module-source-map' : false,
+  output: {
+    path: __dirname,
+    filename: "compiled.js"
+  },
+  watch: isDev,
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        use: [{
+          loader: 'eslint-loader',
+        }],
+      }
+    ]
+  },
+  plugins: plugins
+};
