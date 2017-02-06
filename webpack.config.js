1614b2d0e4b63a3c70548e271cb9d0c44a53e08f
John Wineman <jwineman@cloudflare.com>
2017-02-05 20:13:17 -0800

    Removed babel presets in webpack.config.js, moved webpack dependency local so we can version it.

--- webpack.config.js
+++ webpack.config.js
@@ -25,9 +25,6 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
       },
       {
         test: /\.js$/,
