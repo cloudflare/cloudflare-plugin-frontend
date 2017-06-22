60d061b9f53a6a989ad6506faa856de9d80e4e60
Sachin <sachin@cloudflare.com>
2017-06-22 14:59:46 -0700

    Updated tests for channel generator

--- src/test/selectors/generateChannelLinkTest.js
+++ src/test/selectors/generateChannelLinkTest.js
@@ -7,25 +7,23 @@ describe('generateChannelLink', function() {
     ).toBe(
       'htttp://www.example.com?Param1=param&channel=Integration:wordpress'
     );
-  });
-  it('should return new cpanel uri', function() {
-    expect(generateChannelLink('htttp://www.example.com', 'cpanel')).toBe(
-      'htttp://www.example.com?channel=Integration:cpanel'
-    );
-  });
-  it('should return new magento uri', function() {
-    expect(generateChannelLink('htttp://www.example.com', 'magento')).toBe(
-      'htttp://www.example.com?channel=Integration:magento'
-    );
-  });
-  it('should return any arbitary channel', function() {
-    expect(
-      generateChannelLink('htttp://www.example.com', 'someNewChannel')
-    ).toBe('htttp://www.example.com?channel=Integration:someNewChannel');
-  });
-  it('should return same url when no integrationName', function() {
-    expect(generateChannelLink('htttp://www.example.com')).toBe(
-      'htttp://www.example.com'
-    );
+
+    it('should return any arbitary channel', function() {
+      expect(
+        generateChannelLink('htttp://www.example.com', 'someNewChannel')
+      ).toBe('htttp://www.example.com?channel=Integration:someNewChannel');
+    });
+
+    it('should return same url when no integrationName', function() {
+      expect(generateChannelLink('htttp://www.example.com', '')).toBe(
+        'htttp://www.example.com'
+      );
+    });
+
+    it('should return same url when no integrationName', function() {
+      expect(generateChannelLink('htttp://www.example.com')).toBe(
+        'htttp://www.example.com'
+      );
+    });
   });
 });
