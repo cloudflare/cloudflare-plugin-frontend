88f9a112bf503835eb89801b1847de56786004ce
Yevgen Safronov <yevgen@cloudflare.com>
2020-11-11 17:59:24 +0000

    APO-147 support of subdomains for APO

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,19 +1,3 @@
-// Source: http://stackoverflow.com/a/23945027/4335588
-function extractDomain(url) {
-  var domain;
-  // find & remove protocol (http, ftp, etc.) and get domain
-  if (url.indexOf('://') > -1) {
-    domain = url.split('/')[2];
-  } else {
-    domain = url.split('/')[0];
-  }
-
-  // find & remove port number
-  domain = domain.split(':')[0];
-
-  return domain;
-}
-
 function beginsWith(needle, haystack) {
   return haystack.substr(0, needle.length) == needle;
 }
@@ -23,7 +7,7 @@ function endsWith(str, suffix) {
 }
 
 export function isSubdomain(selectedZoneName) {
-  var currentDomainName = extractDomain(document.URL);
+  var currentDomainName = new URL(document.URL).hostname;
 
   if (
     endsWith(currentDomainName, selectedZoneName) &&
