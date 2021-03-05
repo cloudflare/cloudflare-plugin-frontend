9cdb215a12220cb262b28ce257082e1bd47c5e7a
Jacob Bednarz <jacob.bednarz@gmail.com>
2021-03-05 14:54:19 +1100

    swap layout to using two part card and i8n

--- src/containers/AnalyticsPage/AnaltyicsPage.js
+++ src/containers/AnalyticsPage/AnaltyicsPage.js
@@ -1,40 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { FormattedMessage, injectIntl } from 'react-intl';
-import { Heading } from 'cf-component-heading';
-import { Card, CardSection, CardContent } from 'cf-component-card';
+import { Card, CardSection, CardContent, CardControl } from 'cf-component-card';
+import { Button } from 'cf-component-button';
 
 class AnaltyicsPage extends Component {
   render() {
-    // Use I18n for messaging
-    const { formatMessage } = this.props.intl;
-
     let { activeZone } = this.props;
     const accountId = activeZone.account.id;
     const analyticsDeepLink = `https://dash.cloudflare.com/${accountId}/${activeZone.name}/analytics`;
+
     return (
       <div>
-        <Heading size={1}>
-          <FormattedMessage id="container.analyticsPage.title" />
-        </Heading>
-        <div style={{ backgroundColor: '#FFFFFF' }}>
-          <Card>
-            <CardSection>
-              <CardContent title="Zone Analytics">
-                <hr style={{ margin: '1rem 0' }} width="100%" />
-                <div style={{ textAlign: 'center' }}>
-                  <a href={analyticsDeepLink} target="_blank">
-                    {'Click Here, What do we want to say here?'}
-                  </a>
-                </div>
-              </CardContent>
-            </CardSection>
-          </Card>
-        </div>
+        <Card>
+          <CardSection>
+            <CardContent title={<FormattedMessage id="container.analyticCard.graphql.header" />}>
+              <p>
+                <FormattedMessage id="container.analyticCard.graphql.overview" />
+              </p>
+            </CardContent>
+            <CardControl>
+              <Button type="primary" onClick={function(){window.open(analyticsDeepLink)}}>
+                <FormattedMessage id="container.analyticCard.graphql.linkMessage" />
+              </Button>
+            </CardControl>
+          </CardSection>
+        </Card>
       </div>
     );
   }
 }
+
 function mapStateToProps(state) {
   return {
     activeZone: state.activeZone
